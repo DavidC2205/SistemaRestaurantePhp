cd03b3bdd372066c1a5642d5ae869bb568b71a42
DavidC2205 <davidcastillo2205@gmail.com>
2023-01-09 12:07:17 -0500

    SistemaRestaurante Php , primera subida

---
+++ botanero-ventas-main/src/components/Pie.vue
@@ -0,0 +1,35 @@
+<template>
+    <section  style="padding-top: 3em;  bottom: 0;">
+        <footer class="footer is-primary fondo">
+          
+                <p class="titulo content has-text-centered title is-3 has-text-white">
+                    <strong>{{ datosLocal. nombre }}</strong>. Teléfono <strong>{{ datosLocal.telefono }}</strong>
+                </p>  
+            </div>
+
+        </footer>
+    </section>
+</template>
+<script>
+import HttpService from '../Servicios/HttpService'
+export default {
+    name: "Pie",
+
+    data: () =>({
+        datosLocal: {}
+    }),
+
+    mounted(){
+        this.obtenerDatos()
+    },
+
+    methods: {
+        obtenerDatos(){
+            HttpService.obtener("obtener_datos_local.php")
+            .then(resultado => {
+                this.datosLocal = resultado
+            })
+        }
+    }
+}
+</script>
